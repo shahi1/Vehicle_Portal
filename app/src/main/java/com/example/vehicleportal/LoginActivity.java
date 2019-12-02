2c56c2902386a45e7184cb08e01bd1feaf306906
shahi1 <49000299+shahi1@users.noreply.github.com>
2019-12-02 14:05:22 +0545

    login

---
+++ app/src/main/java/com/example/vehicleportal/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.vehicleportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.vehicleportal.Loginclass;
+import com.example.vehicleportal.R;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText email,password;
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        email=findViewById(R.id.editText);
+        password=findViewById(R.id.editText2);
+        login=findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Loginclass login=new Loginclass(email.getText().toString(),password.getText().toString());
+                if (login.email.equals("admin") && login.password.equals("admin")){
+                    Intent intent=new Intent(LoginActivity.this,DashboardActivity.class);
+                    startActivity(intent);
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/vehicleportal/Loginclass.java
@@ -0,0 +1,28 @@
+package com.example.vehicleportal;
+
+
+public class Loginclass extends LoginActivity {
+
+    String email,password;
+
+    public Loginclass(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
