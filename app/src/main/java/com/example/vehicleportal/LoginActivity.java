83cd91be342296a0927b83ad4c58dbec946e146c
shahi1 <49000299+shahi1@users.noreply.github.com>
2019-12-12 08:54:50 +0545

    Splash Screen

--- app/src/main/java/com/example/vehicleportal/LoginActivity.java
+++ app/src/main/java/com/example/vehicleportal/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.vehicleportal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,24 +20,34 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        email=findViewById(R.id.editText);
-        password=findViewById(R.id.editText2);
-        login=findViewById(R.id.button);
+        email = findViewById(R.id.etUsername);
+        password = findViewById(R.id.etPassword);
+        login = findViewById(R.id.button);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Loginclass login=new Loginclass(email.getText().toString(),password.getText().toString());
-                if (login.email.equals("admin") && login.password.equals("admin")){
-                    Intent intent=new Intent(LoginActivity.this,DashboardActivity.class);
+                Loginclass login = new Loginclass(email.getText().toString(), password.getText().toString());
+                if (login.email.equals("admin") && login.password.equals("admin")) {
+                    Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
                     startActivity(intent);
+                    SaveUsernamePassword();
 
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
     }
-}
+        private void SaveUsernamePassword(){
+            SharedPreferences sharedPreferences=getSharedPreferences("User",MODE_PRIVATE);
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+
+            editor.putString("username",email.getText().toString());
+            editor.putString("password",password.getText().toString());
+            editor.commit();
+            Toast.makeText(this, "Successfully Saved", Toast.LENGTH_SHORT).show();
+        }
+    }
+
